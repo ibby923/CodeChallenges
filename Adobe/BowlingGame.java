fab612e4a4dda52376db943e491ea974b2bb01db
Ibby <ijumkh01@students.poly.edu>
2012-09-25 23:13:17 -0400

    First working run of the game

--- Adobe/BowlingGame.java
+++ Adobe/BowlingGame.java
@@ -1,14 +1,15 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 public class BowlingGame {
 	
-int score;
-int frameNumber;
-int rollNumber;
-int frameScore;
-int extraRolls = 0;
+private int score;
+private int frameNumber;
+private int rollNumber;
+private int frameScore;
+private int extraRolls = 0;
 
 //There has to be a better way to store this!
 ArrayList<HashMap<String,String>> FrameList;
@@ -26,37 +27,11 @@ public BowlingGame() {
 }
 
 public static void main(String [] args) {
-	
 	BowlingGame game = new BowlingGame();
-	game.roll(10);
-	
-	game.roll(10);
-	
-	game.roll(10);
-	
-	game.roll(10);
-	
-	game.roll(6);
-	game.roll(2);
-	
-	game.roll(0);
-	game.roll(10);
-	
-	game.roll(10);
-	
-	game.roll(10);
-	
-	game.roll(10);
-	
-	game.roll(1);
-	
 	game.roll(9);
-	
-	game.roll(10);
-
-	game.getScore();
+	game.isFinished();
 	game.printBalls();
-	
+	System.out.println(game.getScore());
 }
 
 public void roll(int knocked) {
@@ -133,7 +108,7 @@ public void roll(int knocked) {
 					rollNumber--;
 				}
 				else if(knocked+frameScore == 10) {
-					addValueToFrameList("Sparse",frameScore+","+knocked,true,0, true);
+					addValueToFrameList("Spare",frameScore+","+knocked,true,0, true);
 				}
 				
 				else {
@@ -147,13 +122,99 @@ public void roll(int knocked) {
 
 
 public boolean isFinished() {
-	return true;
+	if(FrameList.size() > 9) {
+		String type = (FrameList.get(9).entrySet().iterator().next().getKey());
+		if(type.equals("In Progress")) return false;
+		else if(type.equals("Spare")) {
+			if(FrameList.size() == 11) return true;
+			else return false;
+		}
+		else {
+			if(FrameList.size() == 12) return true;
+			else return false;
+		}
+	}
+	else return false;
+	
 }
 
 
 public int getScore() {
-	System.out.println(FrameList.size()+ " Frames Recorded");
-	return 0;
+	score = 0;
+	int FrameSize = FrameList.size();
+	//System.out.println(FrameSize);
+	for(int i = 0; i < FrameSize; i++) {
+		
+		
+		String Type = (FrameList.get(i).entrySet().iterator().next().getKey());
+		//System.out.println(type);
+	
+		
+		if(Type.equals("Strike")) {
+			score += 10;
+			if(i+1 < FrameSize) {
+				String secondType = (FrameList.get(i+1).entrySet().iterator().next().getKey());
+				//System.out.println(secondType);
+				if(secondType.equals("In Progress")) {
+					String numby = (FrameList.get(i+1).entrySet().iterator().next().getValue());
+					score += Integer.parseInt(numby);
+				}
+				else if(secondType.equals("Spare") || secondType.equals("Open")) {
+					String numby = (FrameList.get(i+1).entrySet().iterator().next().getValue());
+					score += (Integer.parseInt(numby.split(",")[0])+Integer.parseInt(numby.split(",")[1]));
+					continue;
+				}
+				else if(secondType.equals("Strike")){
+					score += 10;
+					if(i+2 < FrameSize) {
+						String thirdType = (FrameList.get(i+2).entrySet().iterator().next().getValue());
+						if(thirdType.equals("Strike")) score += 10;
+						else score += (Integer.parseInt(thirdType.split(",")[0]));
+					}
+				}
+					else {
+						score += (Integer.parseInt(FrameList.get(10).entrySet().iterator().next().getValue()) + Integer.parseInt(FrameList.get(11).entrySet().iterator().next().getValue()));
+					}
+				
+			}
+		}
+		
+		
+		else if(Type.equals("In Progress")) {
+			String numby = (FrameList.get(i).entrySet().iterator().next().getValue());
+			score+= Integer.parseInt(numby.split(",")[0]);
+		}
+		
+		
+		else if(Type.equals("Spare")) {
+			String numby = (FrameList.get(i).entrySet().iterator().next().getValue());
+			score += (Integer.parseInt(numby.split(",")[0])+Integer.parseInt(numby.split(",")[1]));
+			if(i+1 < FrameSize) {
+				String othy = (FrameList.get(i+1).entrySet().iterator().next().getKey());
+				if(othy.equals("Strike")) score += 10;
+				else {
+					String lastnumby = (FrameList.get(i+1).entrySet().iterator().next().getValue());
+					score += Integer.parseInt(lastnumby.split(",")[0]);
+				}
+			}
+		}
+		
+		else if(Type.equals("Open")){
+			String numby = FrameList.get(i).entrySet().iterator().next().getValue();
+			score += (Integer.parseInt(numby.split(",")[0])+Integer.parseInt(numby.split(",")[1]));
+		}
+		
+		else {
+			break;
+		}
+		
+	}
+	return score;
+}
+
+
+public void printBalls() {
+	System.out.println(FrameList.toString());
 }
 
 
@@ -169,10 +230,49 @@ public void addValueToFrameList(String type,String score,boolean removeLast,int 
 	this.frameScore = frameScore; 
 	FrameList.add(myMap);
 }
-
-
-public void printBalls() {
-	System.out.println(FrameList.toString());
 }
 
-}
+
+
+/*
+TEST MODULE
+ 
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:0
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4:80 
+9 1 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:28
+10 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:28
+9 1 10 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:48
+10 9 1 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:48
+10 10 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:57
+10 10 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0:60
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 9 1 9:19 
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10 8 1:19
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10 9 1:20 
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 9 1 10:20
+10 10 10 10 10 10 10 10 10 10 10 10 :300
+	 
+	BowlingGame game = new BowlingGame();
+	String input =  "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:0  4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4:80  9 1 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:28  10 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:28  9 1 10 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:48  10 9 1 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:48  10 10 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0:57  10 10 10 0 0 0 0 0 0 0 0 0 0 0 0 0 0:60  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 9 1 9:19  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10 8 1:19  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 10 9 1:20  0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 9 1 10:20  10 10 10 10 10 10 10 10 10 10 10 10 :300";
+	String t[] =  input.split("  ");
+	int i = 0;
+while(i<t.length) {	
+	String Stringy[] = t[i].split(":");
+	String onemore[] = Stringy[0].split(" ");
+	//System.out.println("Input ==> "+Stringy[0]);
+	game = new BowlingGame();
+	for ( int k =0 ; k < onemore.length; k ++) {
+		
+		//System.out.println(Integer.parseInt(onemore[k]));
+		game.roll(Integer.parseInt(onemore[k]));
+	}
+	//game.printBalls();
+		System.out.println(game.getScore());
+		i++;
+		}
+		
+	
+	
+	//System.out.println(game.isFinished());
+	
+	//System.out.println(game.getScore());
+ */
